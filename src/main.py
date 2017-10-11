5caf394059eeec2139a2508cd0813a337fcfc477
Pratik <pratik.iem01@gmail.com>
2017-10-11 11:51:41 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,9 +8,29 @@ customSchema = StructType([ \
     StructField("tags", StringType(), True), \
     StructField("body", StringType(), True)])
 
-postXml = sc.textFile("spark_data/Posts.xml")
+postXml = sc.textFile("../data/Posts.xml")
           .map(lambda x:x.strip())
           .filter(lambda x:x.startswith("<?xml version=")==False)
           .filter(lambda x:x!="<posts>" and x!="</posts>")
           
 xml = spark.createDataFrame(postXml.map(parse).filter(lambda x:x!=None), customSchema)
+
+
+"""
+Now we can use the xml rdd to build our model
+"""
+
+# Custum function to generete an output label
+def sqlFunc(val):
+    if(val.find("brew")>-1):
+        return 1.0
+    else:
+        return 0.0
+
+udfFunc = udf(sqlFunc, FloatType())
+postDf = xml.withColumn("label",udfFunc("tags"))
+
+# we build two rdds, positive contains the set of instances where we have a tag match, and negative contains the remaining instances
+positive=postDf.filter(postDf.label>0.0)
+negative=postDf.filter(postDf.label<1.0)
+
